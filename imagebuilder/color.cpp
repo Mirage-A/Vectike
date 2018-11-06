35f65786a9fad13801f51f68b236e9f258389ac8
deimosin <grisha_fil@mail.ru>
2018-11-06 19:13:26 +0300

    сделал функцию CombineWith (но это не точно)

--- imagebuilder/color.cpp
+++ imagebuilder/color.cpp
@@ -9,5 +9,12 @@ Color::Color(double alpha, double r, double g, double b) {
 }
 
 Color Color::CombineWith(Color new_color) const {
-    return new_color;
+    double k;
+    Color ans(0,0,0,0);
+    k = new_color.alpha_ + (1 - this->alpha_) * (1 - new_color.alpha_);
+    ans.r_ = this->r_ * this->alpha_ + (new_color.r_ - this->alpha_ * this->r_) * k;
+    ans.g_ = this->g_ * this->alpha_ + (new_color.g_ - this->alpha_ * this->g_) * k;
+    ans.b_ = this->b_ * this->alpha_ + (new_color.b_ - this->alpha_ * this->b_) * k;
+    ans.alpha_ = k;
+    return ans;
 }
