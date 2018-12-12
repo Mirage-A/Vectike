c2c791bcd54f4b1a2158019e4c065eb13c0e56dc
Mirage-A <zaitsev.eo@phystech.edu>
2018-12-12 19:13:40 +0300

    encoding fix #2

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine, int nCmdShow) {
     setlocale(LC_ALL, "Russian");
+    SetConsoleCP(866);
+    SetConsoleOutputCP(866);
     std::cout << "Hello, World!" << std::endl;
     EditorFrame editor_frame(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
     editor_frame.CloseWindow();
