c04dc3fb04bba225b9b278a4dd94ac90dc553a4c
Mirage-A <zaitsev.eo@phystech.edu>
2018-12-12 19:10:50 +0300

    encoding fix

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 // Точка входа в программу - функция WinMain
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine, int nCmdShow) {
+    setlocale(LC_ALL, "Russian");
     std::cout << "Hello, World!" << std::endl;
     EditorFrame editor_frame(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
     editor_frame.CloseWindow();
